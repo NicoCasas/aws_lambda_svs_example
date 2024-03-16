c5517d21e2a750a0dc1e581c4ba5305b199fadc3
NicoCasas <nicolas.casas.250@gmail.com>
2024-03-15 23:09:18 -0300

    fix: some bugs

--- handler.py
+++ handler.py
@@ -4,13 +4,22 @@ from scrapper import get_top_n_majors_three_pointers_nba
 
 def get_welcome_msg(event):
     # Extract the string from the request body
-    name = event['body']
+    if not event['body'] or not event['body']['name']:
+        return {
+            'statusCode': 400,
+            'body':{
+                'error': 'Invalid body'
+            }
+        }
+    name = event['body']['name']
     
     # Verify if the name contains only alphabetical values
     if not re.match(r'^[a-zA-Z]+$', name):
         return {
             'statusCode': 400,
-            'body': 'Invalid name. Only alphabetical values are allowed.'
+            'body': {
+                'error': 'Invalid name. Only alphabetical values are allowed.'
+            }
         }
 
     welcome_message = 'hola ' + name
@@ -22,9 +31,24 @@ def get_welcome_msg(event):
     }
 
 
+def get_scorers():
+    top_scorers = get_top_n_majors_three_pointers_nba(5)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(top_scorers)
+    }
+
+
 def lambda_handler(event, context):
-    http_method = event['httpMethod']
+    print(f'event -> {event}')
     
+    if not event['httpMethod']:
+        return {
+            'statusCode': 400,
+            'error': 'Bad Request'
+        }
+    
+    http_method = event['httpMethod']
     # Check if the request method is POST
     if http_method not in ['GET', 'POST']:
         return {
@@ -33,7 +57,7 @@ def lambda_handler(event, context):
         }
     
     if http_method == 'GET':
-        return get_top_n_majors_three_pointers_nba(5)
+        return get_scorers()        
     
     if http_method == 'POST':
         return get_welcome_msg(event)
